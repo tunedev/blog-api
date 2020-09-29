364278b23f5a4c05bd338da994724955510df775
Babatunde Sanusi <swisskid95@gmail.com>
2020-09-29 12:43:05 +0100

    setup project file structure and define models

---
+++ Model/Comment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  post_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
+  body: String,
+  created_at: Date,
+  updated_at: Date,
+});
+
+commentSchema.set('toJSON', {
+  transform(document, returnedObj) {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model('Post', commentSchema);
